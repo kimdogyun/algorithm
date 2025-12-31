f393363e665a1d3c372dfc4c1c28f493965d6257
kimdogyun <sydg1214@gmail.com>
2025-12-31 16:11:39 +0900

    a

---
+++ src/A5Dp/A02동전문제.java
@@ -0,0 +1,26 @@
+package A5Dp;
+
+import java.util.Arrays;
+
+public class A02동전문제 {
+    public static void main(String[] args) {
+//        1,4,5원의 동전이 있을떄, 조합하여 13을 만들 수 있는 최소의 개수
+        int [] arr = {1,4,5};
+        int target = 13;
+//        n원을 만드는 최소동전수 : min(dp[n-a],dp[n-b],dp[n-c])+1
+        int [] dp = new int[target+1];
+        for (int i = 1; i <=target ; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j <arr.length ; j++) {
+                if(i-arr[j]>=0 && dp[i-arr[j]]<min){
+                    min = dp[i-arr[j]];
+            }
+            }
+            dp[i] = min+1;
+        }
+        System.out.println(Arrays.toString(dp));
+//        1이 포함되지 않을수 있고, target을 못만들수도 있는 경우의 동전 문제
+//        동전2(2294) : 백준
+        }
+
+    }
